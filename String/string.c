f70213ceecfa39938c894b747058b256f15304d9
kadelcode <kadelcode@gmail.com>
2022-09-16 23:12:58 +0100

    Working with C strings

---
+++ String/string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+A string is an array of characters.
+A string is enclosed with double quotes
+Characters are enclosed by single quotes
+*/
+
+void main() {
+    char name[3] = {'A','Y','0'};
+    printf("%s", name);
+}
