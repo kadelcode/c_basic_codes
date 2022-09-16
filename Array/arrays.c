b843dfa2f770e065997286181a400613ac8c4e72
kadelcode <kadelcode@gmail.com>
2022-09-16 22:07:26 +0100

    Arrays

---
+++ Array/arrays.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main() {
+    // One Dimensional Array
+    int arr[5]; //array declaration
+    int arr[5] = {2,3,56,41,100}; //array initialization
+
+
+    // Two Dimensional Array
+    int num[3][3];
+    int num[3][3] = {{1,2,3}, {4,5,6}, {6,7,8}};
+    printf('%d',arr[3]);
+
+    // Multi Dimensional Array
+    int x[2][3][4]; // declaration
+}
